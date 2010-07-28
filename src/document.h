10887d6e863f5219962430793061877cd6ae4deb
Jon A. Cruz <jon@joncruz.org>
2010-07-27 22:07:32 -0700

    Applied fix for sp_document_maybe_done() that was holding pointers to internal temp buffers.

--- src/document.h
+++ src/document.h
@@ -96,7 +96,8 @@ struct SPDocument : public Inkscape::GC::Managed<>,
     SPDocumentPrivate *priv;
 
     /// Last action key
-    const gchar *actionkey;
+    Glib::ustring actionkey;
+
     /// Handler ID
     guint modified_id;
     
