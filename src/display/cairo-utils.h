2bdfd9aa056dd1b44870134cbb666d352d1f824d
Krzysztof Kosiński <tweenk.pl@gmail.com>
2011-04-09 03:44:07 +0200

    Slightly improve EXTRACT_ARGB32 macro

--- src/display/cairo-utils.h
+++ src/display/cairo-utils.h
@@ -126,10 +126,10 @@ void feed_pathvector_to_cairo (cairo_t *ct, Geom::PathVector const &pathv);
 
 #define EXTRACT_ARGB32(px,a,r,g,b) \
     guint32 a, r, g, b; \
-    a = (px & 0xff000000) >> 24; \
-    r = (px & 0x00ff0000) >> 16; \
-    g = (px & 0x0000ff00) >> 8;  \
-    b = (px & 0x000000ff);
+    a = ((px) & 0xff000000) >> 24; \
+    r = ((px) & 0x00ff0000) >> 16; \
+    g = ((px) & 0x0000ff00) >> 8;  \
+    b = ((px) & 0x000000ff);
 
 #define ASSEMBLE_ARGB32(px,a,r,g,b) \
     guint32 px = (a << 24) | (r << 16) | (g << 8) | b;
