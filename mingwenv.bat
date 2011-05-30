a3315c8c14e942240f4f5b885d3909fe78efbdd6
Jasper van de Gronde <jasper.vandegronde@gmail.com>
2011-05-30 09:16:13 +0200

    Corrected accidental mingwenv.bat modification.

--- mingwenv.bat
+++ mingwenv.bat
@@ -1,7 +1,6 @@
 @echo Setting environment variables for MinGw build of Inkscape
-
-IF "%DEVLIBS_PATH%"=="" set DEVLIBS_PATH=e:\Inkscape\devlibs
-IF "%MINGW_PATH%"=="" set MINGW_PATH=c:\mingw32
+IF "%DEVLIBS_PATH%"=="" set DEVLIBS_PATH=c:\devlibs
+IF "%MINGW_PATH%"=="" set MINGW_PATH=c:\mingw
 set MINGW_BIN=%MINGW_PATH%\bin
 set PKG_CONFIG_PATH=%DEVLIBS_PATH%\lib\pkgconfig
 set PATH=%DEVLIBS_PATH%\bin;%DEVLIBS_PATH%\python;%MINGW_BIN%;%PATH%
